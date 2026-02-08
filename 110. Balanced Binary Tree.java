01e7cb5118043364bb950ee2a4e6176675e3d3bf
Naman <namanjadiya2006@gmail.com>
2026-02-08 23:20:42 +0530

    Create 110. Balanced Binary Tree.java

---
+++ 110. Balanced Binary Tree.java
@@ -0,0 +1,24 @@
+class Solution {
+    public static int check(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+
+        int left =check(root.left);
+        if(left==-1){
+            return -1;
+        }
+        int right=check(root.right);
+        if(right==-1){
+            return -1;
+        }
+        if(Math.abs(right-left)>1){
+            return -1;
+        }
+        return 1+Math.max(left,right);
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return check(root)!=-1;
+    }
+}
