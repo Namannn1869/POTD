55a15dbde30d980dd2caec9d112f70c8f88a6743
Naman <namanjadiya2006@gmail.com>
2026-03-08 23:24:19 +0530

    Create 1980. Find Unique Binary String.java

---
+++ 1980. Find Unique Binary String.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            result.append(nums[i].charAt(i) == '1' ? '0' : '1');
+        }
+        return result.toString(); 
+    }
+}
