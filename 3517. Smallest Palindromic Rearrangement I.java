6bdc9cc6b62789e6344d302951c25c58dc0796c9
Naman <namanjadiya2006@gmail.com>
2026-07-28 16:21:12 +0530

    Add solution for smallest palindromic rearrangement
    Implement a method to find the smallest palindromic rearrangement of a string.

---
+++ 3517. Smallest Palindromic Rearrangement I.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public String smallestPalindrome(String s) {
+        int n = s.length();
+        int mid = n / 2;
+
+        char[] arr = s.toCharArray();
+
+        // Sort the first half
+        Arrays.sort(arr, 0, mid);
+
+        // Copy the first half to the second half in reverse order
+        for (int i = 0; i < mid; i++) {
+            arr[n - 1 - i] = arr[i];
+        }
+
+        return new String(arr);
+    }
+}
